1d6fc426ffec9f6c4f6fb7e134f2a35810372b03
Dobre-Mircea <dobremirceaionut@gmail.com>
2020-03-19 02:53:42 +0100

    Revert "Initial commit"
    This reverts commit e54fb04bbe54694eb3bb83df114dceaf2a598ddc.

--- src/model/BankModel.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public interface BankModel extends PropertyChangeStuff
-{
-}
--- src/model/PropertyChangeStuff.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.beans.PropertyChangeListener;
-
-public interface PropertyChangeStuff
-{
-    void addListener(String propertyName, PropertyChangeListener listener);
-    void removeListener(String propertyName, PropertyChangeListener listener);
-}
